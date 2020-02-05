d424fb130e4620fcc4e7777fa06de367c423f924
Ard-Jan van Etten <arte@linux.fritz.box>
2020-02-05 18:13:01 +0100

    lol

--- main.js
+++ main.js
@@ -7,11 +7,15 @@ canvas.height = mcbheight;
 var ctx = canvas.getContext('2d');
 var mousedown=false;
 
+var a;
+var b;
 
 var bw = mcbwidth/5;
-
-var x = mcbwidth/2-bw/2;
-var y = mcbheight/2;
+const xo = mcbwidth/2-bw/2;
+const yo = mcbheight/2;
+//xo+bw/2
+var x = Math.round(xo+Math.random()*bw*0.8+bw*0.1);
+var y = yo;
 
 ctx.fillStyle='rgb(100,100,100)';
 ctx.fillRect(mcbwidth/2-bw/2,mcbheight/2-2,bw,4);
@@ -19,13 +23,27 @@ ctx.fillRect(mcbwidth/2-bw/2,mcbheight/2-2,bw,4);
 ctx.fillStyle='rgb(200,200,200)';
 ctx.lineWidth=5;
 
-function draw(x,y){
+ctx.font = Math.round(mcbwidth/20) + 'px Times New Roman';
+
+
+function draw(x,y,realy){
+  ctx.fillStyle='rgb(200,200,200)';
   ctx.clearRect(0,0,mcbwidth,mcbheight);
-  ctx.fillRect(mcbwidth/2-bw/2,mcbheight/2-2,bw,4);
+  ctx.fillRect(xo,yo-2,bw,4);
   ctx.beginPath();
   ctx.arc(x,y,20,0,2*Math.PI);
   ctx.fill();
   ctx.stroke();
+  ctx.fillStyle='rgb(0,0,0)';
+
+  a=x/mcbwidth*500-200;
+  b=-y/mcbheight*500+250;
+  if(realy && b != 0){
+    ctx.fillText(Math.round(a) + ' + ' + Math.round(b) + 'i%',0,Math.round(mcbwidth/20));
+  }else{
+    ctx.fillText(Math.round(a) + '%',0,Math.round(mcbwidth/20));
+  }
+
 }
 
 canvas.onmousedown=function(){
@@ -37,7 +55,13 @@ canvas.onmouseup=function(){
 
 document.onmousemove=function(event){
   if(mousedown){
-    draw(event.offsetX,event.offsetY);
+    x=event.offsetX;
+    y=event.offsetY;
+    if(x>xo && x<xo+bw && y<yo+10 && y>yo-10){
+      draw(x,yo,false);
+    }else{
+      draw(x,y,true);
+    }
   }
 }
 
