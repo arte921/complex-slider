ce4f6cdbf87a81df0d6dfd2907e4e2f9ec8c4f04
Ard-Jan van Etten <arte@linux.fritz.box>
2020-02-05 16:14:20 +0100

    much

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log('hi');
+var mcbwidth = document.body.clientWidth;
+var mcbheight = document.body.clientHeight;
+
+var canvas = document.getElementById('canvas');
+canvas.width = mcbwidth;
+canvas.height = mcbheight;
+var ctx = canvas.getContext('2d');
+
+ctx.fillStyle='rgb(100,100,100)';
+
+var bw = mcbwidth/5;
+
+ctx.fillRect(mcbwidth/2-bw/2,mcbheight/2+2,bw,4);
+
+ctx.fillStyle='rgb(200,200,200)';
+ctx.lineWidth=5;
+
+
+
+
+function draw(x,y){
+  ctx.beginPath();
+  ctx.arc(x,y,20,0,2*Math.PI);
+  ctx.fill();
+  ctx.stroke();
+}
+
+draw(20,20);
