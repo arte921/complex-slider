3772b05471e4856d60724c4d90a2cf4b7ff9258b
Ard-Jan van Etten <arte@linux.fritz.box>
2020-02-05 17:10:04 +0100

    e

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var canvas = document.getElementById('canvas');
 canvas.width = mcbwidth;
 canvas.height = mcbheight;
 var ctx = canvas.getContext('2d');
-
+var mousedown=false;
 
 
 var bw = mcbwidth/5;
@@ -24,6 +24,19 @@ function draw(x,y){
   ctx.arc(x,y,20,0,2*Math.PI);
   ctx.fill();
   ctx.stroke();
+}
+
+canvas.onmousedown=function(){
+  mousedown=true;
+}
+canvas.onmouseup=function(){
+  mousedown=false;
+}
+
+document.onmousemove=function(event){
+  if(mousedown){
+    draw(event.offsetX,event.offsetY);
+  }
 }
 
 draw(x,y);
